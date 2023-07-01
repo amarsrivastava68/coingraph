cb09ac80dc6588019feafa984672fbe822cb91fe
amarsrivastava68 <70334464+amarsrivastava68@users.noreply.github.com>
2023-07-01 22:37:16 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
-const avatarSrc = "../assets/amar.jpg";
+const amar = "../assets/amar.jpg";
 
 const Footer = () => {
   return (
